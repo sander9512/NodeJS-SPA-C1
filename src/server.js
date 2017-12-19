a8ea9180c62a6f209e2d74c4d03ee024f11ccaff
Lars Smans <mhsmans@avans.nl>
2017-12-19 12:10:30 +0100

    Opzet voor server

--- src/server.js
+++ src/server.js
@@ -4,10 +4,60 @@ var bodyparser = require('body-parser');
 var logger = require('morgan');
 
 var config = require('./config/env/env');
+var mongodb = require('./config/mongo.db');
 
 var app = express();
 
 app.use(bodyParser.urlencoded({
   'extended' : 'true'
 }));
-app.use()
+app.use(bodyParser.json());
+app.use(bodyParser.json({
+  type: 'application/vnd.api+json'
+}));
+
+app.set('port', (process.env.PORT | config.env.webPort));
+app.set('env', (process.env.ENV | 'development'));
+
+app.use(logger('dev'));
+
+// CORS headers
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN || 'http://localhost:4200');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+
+// app.use('/api', ...);
+
+// error handler express-jwt errors
+app.use(function (err, req, res, next) {
+    var error = {
+        message: err.message,
+        code: err.code,
+        name: err.name,
+        status: err.status
+    }
+    res.status(401).send(error);
+});
+
+app.use('*', function(req, res) {
+  res.status(400);
+  res.json({
+    'error' : 'Waarom kijk jij op deze route?'
+  });
+});
+
+app.listen(config.env.webPort, function () {
+    console.log('De server luistert op port ' + app.get('port'));
+    // console.log('Zie bijvoorbeeld http://localhost:3000/api/...');
+});
+
+module.exports = app;
