567d480bf161afbae21a51e2d9cabc171d9ca30a
Sander Schep <s.c.schep@gmail.com>
2017-12-20 12:33:12 +0100

    Changed routes to web api -- fully working now

--- src/routes/sporthalhuren.routes.v1.js
+++ src/routes/sporthalhuren.routes.v1.js
@@ -1,88 +1,82 @@
 var express = require('express');
 var routes = express.Router();
 var request = require('request');
+//Pas deze webUrl aan aan jouw lokale port
+const webUrl = 'http://localhost:50719/api/v1/';
 
-routes.get('/halls', function (req, res, next) {
-    request('http://localhost:52070/api/v1/Halls', { json: true}, (err, res, body) => {
+routes.get('/halls', function (req, res) {
+    request(webUrl + 'Halls', { json: true}, (err, body) => {
         if(err) {
             return console.log(err);
         } else {
-            res.status(200).json(body);
-            next();
+            res.status(200).json(body.body);
         }
     })
 });
-routes.get('/halls/:id', function (req, res, next) {
-    request('http://localhost:52070/api/v1/Halls/' + req.params.id, { json: true}, (err, res, body) => {
+routes.get('/halls/:id', function (req, res) {
+    request(webUrl + 'Halls/' + req.params.id, { json: true}, (err, body) => {
         if(err) {
             return console.log(err);
         } else {
-            res.status(200).json(body);
-            next();
+            res.status(200).json(body.body);
         }
     })
 });
 //Zalen in sporthal ophalen
-routes.get('/halls/:id/rooms', function (req, res, next) {
-    request('http://localhost:52070/api/v1/Halls/' + req.params.id + '/Rooms', { json: true}, (err, res, body) => {
+routes.get('/halls/:id/rooms', function (req, res) {
+    request(webUrl + 'Halls/' + req.params.id + '/Rooms', { json: true}, (err, body) => {
         if(err) {
             return console.log(err);
         } else {
-            res.status(200).json(body);
-            next();
+            res.status(200).json(body.body);
         }
     })
 });
 //Openingstijden ophalen
-routes.get('/halls/:id/times', function (req, res, next) {
-    request('http://localhost:52070/api/v1/Halls/' + req.params.id + '/Times', { json: true}, (err, res, body) => {
+routes.get('/halls/:id/times', function (req, res) {
+    request(webUrl + 'Halls/' + req.params.id + '/Times', { json: true}, (err, body) => {
         if(err) {
             return console.log(err);
         } else {
-            res.status(200).json(body);
-            next();
+            res.status(200).json(body.body);
         }
     })
 });
 //faciliteiten van een sporthal ophalen, nog te maken in web api
-routes.get('/halls/:id/facilities', function (req, res, next) {
-    request('http://localhost:52070/api/v1/Halls/' + req.params.id + '/Facilities', { json: true}, (err, res, body) => {
+routes.get('/halls/:id/facilities', function (req, res) {
+    request(webUrl + 'Halls/' + req.params.id + '/Facilities', { json: true}, (err, body) => {
         if(err) {
             return console.log(err);
         } else {
-            res.status(200).json(body);
-            next();
+            res.status(200).json(body.body);
         }
     })
 });
 //beschikbare sporten/huurbare objecten van een sporthal, nog te maken in web api
-routes.get('/halls/:id/activities', function (req, res, next) {
-    request('http://localhost:52070/api/v1/Halls/' + req.params.id + '/Activities', { json: true}, (err, res, body) => {
+routes.get('/halls/:id/activities', function (req, res) {
+    request(webUrl + 'Halls/' + req.params.id + '/Activities', { json: true}, (err, body) => {
         if(err) {
             return console.log(err);
         } else {
-            res.status(200).json(body);
-            next();
+            res.status(200).json(body.body);
         }
     })
 });
-routes.get('/bookings', function (req, res, next) {
-    request('http://localhost:52070/api/v1/Bookings', { json: true}, (err, res, body) => {
+routes.get('/bookings', function (req, res) {
+    request(webUrl + 'Bookings', { json: true}, (err, body) => {
         if(err) {
             return console.log(err);
         } else {
-            res.status(200).json(body);
-            next();
+            res.status(200).json(body.body);
         }
     })
 });
-routes.get('/bookings/:id', function (req, res, next) {
-    request('http://localhost:52070/api/v1/Bookings/' + req.params.id, { json: true}, (err, res, body) => {
+routes.get('/bookings/:id', function (req, res) {
+    request(webUrl + 'Bookings/' + req.params.id, { json: true}, (err, body) => {
         if(err) {
             return console.log(err);
         } else {
-            res.status(200).json(body);
-            next();
+            res.status(200).json(body.body);
         }
     })
 });
